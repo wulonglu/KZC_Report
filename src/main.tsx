63431b0f9e24ee4c4fd6e4e5ec23d3b513286f63
wulonglu <3344930478@qq.com>
2026-08-05 17:03:14 +0800

    main.tsx 集成密码验证门禁

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import { useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import LoginGate from './components/LoginGate'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+function Root() {
+  const [authed, setAuthed] = useState(() => sessionStorage.getItem('kzc_auth') === '1')
+
+  if (!authed) {
+    return <LoginGate onSuccess={() => setAuthed(true)} />
+  }
+
+  return <App />
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<Root />)
